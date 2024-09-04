0fa4c3eddefa8a1bb9f885945113ba2e0460c237
Atta-ul-Shahfi <102184902+shahfiattaul@users.noreply.github.com>
2024-09-04 17:03:21 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 #Catalog
 
 #Adding first product's description
-lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white"
-lovely_loveseat_price = 254.00
+loveseat_description = "Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white"
+loveseat_price = 254.00
 
 #Adding second product's description
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
@@ -19,8 +19,8 @@ sales_tax = .088
 customer_one_total = 0
 customer_one_itemization = " "
 
-customer_one_total += lovely_loveseat_price
-customer_one_itemization += lovely_loveseat_description
+customer_one_total += loveseat_price
+customer_one_itemization += loveseat_description
 
 customer_one_total += luxurious_lamp_price
 customer_one_itemization += luxurious_lamp_description
